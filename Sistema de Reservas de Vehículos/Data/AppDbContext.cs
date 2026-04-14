ad7e35074fb16fb1f634f65bb1d16c193b3af24f
Edwin Felipe <105158452+TheAwful1@users.noreply.github.com>
2026-04-14 11:47:03 -0400

    Agregar archivos de proyecto.

---
+++ Sistema de Reservas de Vehículos/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sistema_de_Reservas_de_Vehículos.Data
+{
+    public class AppDbContext:DbContext
+    {
+        //Acuerdate de poner 
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+       : base(options)
+        {
+        }
+
+    }
+}
---
+++ Sistema de Reservas de Vehículos/Program.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Connections;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Microsoft.EntityFrameworkCore.SqlServer;
+using Sistema_de_Reservas_de_Vehículos.Data;
+using System.Data.Common;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
