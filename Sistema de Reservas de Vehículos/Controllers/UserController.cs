226a725f1cd5ea962a390edc5a75724c5bcdb2e6
Edwin Felipe <105158452+TheAwful1@users.noreply.github.com>
2026-04-14 14:16:57 -0400

    Vistas y controlador a mano creados

---
+++ Sistema de Reservas de Vehículos/Controllers/UserController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Sistema_de_Reservas_de_Vehículos.Models;
+
+namespace Sistema_de_Reservas_de_Vehículos.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly VehicleReservationsDbContext _context;
+
+        public UserController(VehicleReservationsDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var users = _context.Users.ToList();
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Users.Add(user);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(user);
+        }
+    }
+}
